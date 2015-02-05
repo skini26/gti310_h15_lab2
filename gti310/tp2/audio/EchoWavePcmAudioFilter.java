d2fbb6d0dc3effa71f65712b83522a5843de3c5c
Yanis <yanis.ikene.1@etsmtl.net>
2015-02-05 17:17:57 -0500

    Modified filter to process data in batches, need to fix it too

--- gti310/tp2/audio/EchoWavePcmAudioFilter.java
+++ gti310/tp2/audio/EchoWavePcmAudioFilter.java
@@ -31,6 +31,8 @@ public class EchoWavePcmAudioFilter implements AudioFilter {
 	public static final int WAVE_HEADER_SIZE = 44;
 	public static final String FILE_FORMAT = "WAVE";
 	public static final int FILE_TYPE = 1;
+	//Filter 1 MB at a time to not load the entire file in memory
+	public static final int BATCH_SIZE = 1024*1024; 
 	
 	public EchoWavePcmAudioFilter(FileSource input, FileSink output, 
 								int delai, int facteurAttenuation) throws Exception{
@@ -95,6 +97,7 @@ public class EchoWavePcmAudioFilter implements AudioFilter {
 			     ((header[41] & 0xFF) << 8) | ((header[40] & 0xFF)<< 0);
 		System.out.println("Data size : "+dataSize/8+" bytes");
 		
+		
 		//
 		
 	}
@@ -110,33 +113,34 @@ public class EchoWavePcmAudioFilter implements AudioFilter {
 			//Write header
 			writeHeader();
 			
-			//Get Audio Data and process it.
-			byte[] data = input.pop(dataSize);
-			ByteArrayInputStream dataIn = new ByteArrayInputStream(data);
-			
-			int bytePerSubSample = bitsPerSample/8;
-			System.out.println("Bytes per sub sample : "+bytePerSubSample);
-			int bytePerBigSample = numberOfChannels*bytePerSubSample; 
-			System.out.println("Bytes per complete sample : "+bytePerBigSample);
-			
+			byte[] data;
+			int bytesPerSubSample = bitsPerSample/8;
+			System.out.println("Bytes per sub sample : "+bytesPerSubSample);
+			int bytesPerBigSample = numberOfChannels*bytesPerSubSample; 
+			System.out.println("Bytes per complete sample : "+bytesPerBigSample);
 			//Sauter une ligne
 			System.out.println();
 			
-			for(int i = 0; i<data.length; i+=bytePerSubSample){
+			//Get Audio Data by batches and process it.
+			do{
+				
+				data = input.pop(BATCH_SIZE);
+				ByteArrayInputStream dataIn = new ByteArrayInputStream(data);
+
 				
 				//Progress bar
 				System.out.print(".");
 				
-				dataIn.reset();
-				dataIn.skip(i);
-				
-				byte[] sample = new byte[bytePerSubSample];
-				dataIn.read(sample);
-				
-				if(i<bytePerBigSample){
-					output.push(sample);
-				}
-				else{
+				for(int i = 0; i<data.length; i+=bytesPerSubSample){
+					
+					dataIn.reset();
+					dataIn.skip(i);
+					
+					byte[] sample = new byte[bytesPerSubSample];
+					dataIn.read(sample);
+					
+					
+					
 					/*
 					 * L’effet d’écho à implémenter pour ce laboratoire consiste à répéter
 					 *  le signal d’origine, noté x, après un délai de M échantillons. 
@@ -147,14 +151,16 @@ public class EchoWavePcmAudioFilter implements AudioFilter {
 					 */
 					
 					//a.x[n-M]
-					int m = delai*bytePerBigSample;
+					int m = delai*bytesPerBigSample;
 					
 					//x[n] == sample
 					
 					//x[n-m] == delayedSample
 					dataIn.reset();
-					dataIn.skip(i-m);
-					byte[] delayedSample = new byte[bytePerSubSample];
+					int bytesDelay = i-m;
+					if(bytesDelay<0) { bytesDelay = 0; }
+					dataIn.skip(bytesDelay);
+					byte[] delayedSample = new byte[bytesPerSubSample];
 					dataIn.read(delayedSample);
 					
 					//y[n] = x[n] + a.x[n-M] = sample + facteurAttenuation*delayedSample
@@ -172,17 +178,20 @@ public class EchoWavePcmAudioFilter implements AudioFilter {
 					int outSampleInt = sampleInt + (facteurAttenuation*delayedSampleInt);
 					
 					//Convert to byte array
-					byte[] outSample = ByteBuffer.allocate(bytePerSubSample).putShort((short) outSampleInt).array();
+					byte[] outSample = ByteBuffer.allocate(bytesPerSubSample).putShort((short) outSampleInt).array();
 
 					//Write byte array on the file.
 					output.push(outSample);
 					
-				}
 				
-			}
-			dataIn.close();
-		} 
+					
+				}
+				dataIn.close();
+			
+			}while(data != null); 
 		
+			
+		}
 		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
